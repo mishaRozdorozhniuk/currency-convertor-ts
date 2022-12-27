dbdac5365e93ed14f7081c817fa5706c48686c04
mishaRozdoroznhuk <misha.roz.2003@gmail.com>
2022-12-27 23:00:36 +0100

    add type to input and select html elements

--- src/components/CurrencyInput/CurrencyInput.tsx
+++ src/components/CurrencyInput/CurrencyInput.tsx
@@ -1,11 +1,13 @@
 import PropTypes from "prop-types";
 import './CurrencyInput.css';
+import {ChangeEvent} from "react";
 
 const CurrencyInput = (props: any) => {
+
   return (
     <div className="currency-form-group">
-      <input type="text" value={props.amount} onChange={ev => props.onAmountChange(ev.target.value)} />
-      <select value={props.currency} onChange={ev => props.onCurrencyChange(ev.target.value)}>
+      <input type="text" value={props.amount} onChange={(e: ChangeEvent<HTMLInputElement>) => props.onAmountChange(e.target.value)} />
+      <select value={props.currency} onChange={(e: ChangeEvent<HTMLSelectElement>) => props.onCurrencyChange(e.target.value)}>
         {props.currencies.map(((currency: string) => (
           <option key={currency} value={currency}>{currency}</option>
         )))}
